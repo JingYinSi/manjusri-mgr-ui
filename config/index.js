1bc32f112a4331471c6c85410a9ca93b0b911919
clx1608@qq.com <hbdennischen@outlook.com>
2018-03-24 13:46:34 +0800

    vue - components - 使用相对路径发布vue-cli webpack项目 fixed #1

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: './', // set root dir as relative way
 
     /**
      * Source Maps
